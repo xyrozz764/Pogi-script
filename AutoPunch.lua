0432db36bf80cf4aaca09b750e609da6b5bc71f2
xyrozz764 <justmakey@gmail.com>
2025-04-25 20:54:57 +0800

    Update AutoPunch.lua

--- AutoPunch.lua
+++ AutoPunch.lua
@@ -1,1 +1,26 @@
-
+local player = game.Players.LocalPlayer
+‎
+‎local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyrozz764/Pogi-script/main/READMEmd.lua"))()
+‎
+‎local Window = OrionLib:MakeWindow({
+‎    Name = "pogis hub",
+‎    HidePremium = false,
+‎    SaveConfig = true,
+‎    ConfigFolder = "OrionTest"
+‎})
+‎
+‎local Tab = Window:MakeTab({
+‎    Name = "nyek",
+‎    Icon = "rbxassetid://4483345998",
+‎    PremiumOnly = false
+‎})
+‎
+‎local Section = Tab:AddSection({
+‎    Name = "LocalPlayer"
+‎})
+‎
+‎OrionLib:MakeNotification({
+‎    Name = "SHABU",
+‎    Content = miss mo
+‎    Image = "rbxassetid://4483345998",
+‎    Time = 5
